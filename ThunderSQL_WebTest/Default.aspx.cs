5b980f6a386b5d2520e3ba1b11cdac5cd1a56ede
LT <longt@aliyun.com>
2017-08-30 14:39:38 +0800

    Add test for Asp.net

---
+++ ThunderSQL_WebTest/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ThunderSQL;
+
+namespace ThunderSQL_WebTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var context = new DataContext();
+            string sql = "select car_name from cars where car_id=12";
+            string value = context.GetValue<string>(sql);//返回第一行第一列数据;
+            Label1.Text = value;
+        }
+    }
+}
